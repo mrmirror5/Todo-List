04c288b1ef63d91e4e43d0e1d1c91727c89b5d94
Miro <mirstumiro@gmail.com>
2026-03-10 20:31:23 +0200

    Added Local Storage to preserve the TODO list even after refresh.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+//Todo list structure
+let todoList = [];
+
+// Print tasks from local storage on refresh
+readSavedList();
+printTasks()
+
 const inputBox = document.getElementById("input-box");
-const taskBucket = document.getElementById("task-bucket");
 
 //Make Add button addTask
 const addButton = document.getElementById("add-button");
@@ -8,47 +14,89 @@ addButton.addEventListener("click", addTask);
 //Make enter addTask
 document.getElementById("input-box").addEventListener("keypress", function(e){
     if (e.key === "Enter") {
-        addTask()
+
+        readSavedList();
+        // Adds to list
+        addTask();
+        // save to local storage
+        saveTaskList();
+        // print
+        printTasks();
+
     }
 })
 
 
 
+
+
+
 function addTask(){
+    const taskString = document.getElementById("input-box").value;
     if(inputBox.value === ""){
         alert("You must write something!");
     }
     else{
+        todoList.push(taskString);
+        inputBox.value = "";
+    }
+}
+
+
+
+function saveTaskList(){
+    localStorage.setItem("savedTaskList", JSON.stringify(todoList));
+}
+
+function readSavedList(){
+    const storedTasks = localStorage.getItem("savedTaskList")
+    
+    if (storedTasks) {
+        todoList = JSON.parse(storedTasks);
+
+    } else {
+        todoList = [];
+    }
+}
+
+function printTasks(){
+    // Print the todo-list
+    const taskBucket = document.getElementById("task-bucket");
+    taskBucket.innerHTML = ""
+  
+    for (let i=0; i< todoList.length; i++ ) {
+        // Using the following mode instead of .innerHTML just to keep the event listeners.
         const taskBody = document.createElement("div");
         taskBody.classList.add("task-body");
 
         taskBody.innerHTML = `
             <div class="task">
-                <p>${inputBox.value}</p>
+                <p>${todoList[i]}</p>
             </div>
-            <div class="basic-button rm-button">
-                Remove
-            </div>
-            <div class="basic-button done-button">
-                Done
-            </div>       
+            <div class="basic-button rm-button">Remove</div>
+            <div class="basic-button done-button">Done</div>
         `;
+
         //Event listener for removing task
         const removeButton = taskBody.querySelector(".rm-button");
         const doneButton = taskBody.querySelector(".done-button");
 
         removeButton.addEventListener("click", function(){
             document.getElementById("task-not-done-audio").play();
-            taskBody.remove();
+            
+            // Remove from local storage, save and print
+            todoList.splice(i,1)
+            saveTaskList()
+            printTasks()
         });
         doneButton.addEventListener("click", function(){
             document.getElementById("task-done-audio").play();
-            taskBody.remove();
+
+            // Remove from local storage, save and print
+            todoList.splice(i,1)
+            saveTaskList()
+            printTasks()
         });
-        inputBox.value = ""
         taskBucket.appendChild(taskBody);
     }
 }
-
-
-
